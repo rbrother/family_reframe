c39f0a9969d881036b0d00fd1a85f1e7b66ac100
Robert Brotherus <robert@iki.fi>
2019-03-21 19:35:25 +0200

    images working in the person list :-)

--- src/cljs/family_reframe/views.cljs
+++ src/cljs/family_reframe/views.cljs
@@ -2,6 +2,9 @@
   (:require [re-frame.core :as re-frame]
             [family-reframe.subs]))
 
+(defn header []
+  [:div.header "Re-Frame Family Database - Copyright Robert J. Brotherus 2019. Logged in." ])
+
 (defn input-field [ data-path chars ]
   (let [current-value @(re-frame/subscribe [:value data-path])
         dispatcher #(re-frame/dispatch [:set-value data-path (-> % .-target .-value)]) ]
@@ -12,7 +15,9 @@
 (defn filter-fields []
   [:div
    [:div "Filter text"]
-   [:div [input-field [:filter-text]]]])
+   [:div [input-field [:filter-text] 8]]
+   (let [persons @(re-frame/subscribe [:filtered-persons])]
+    [:div (count persons) " persons"])])
 
 (defn format-name [{:keys [first last called orig-last]}]
   (let [show-called (and called (not= called "") (not= called first))]
@@ -20,8 +25,9 @@
      (if show-called (str " (" called ")")) " "
      [:span {:style {:font-weight "bold"}} last]]))
 
-(defn format-image [{:keys [year name]}]
-  name)
+(defn format-image [ person-id { image-name :name }]
+  (if image-name
+    [:img {:src (str "http://www.brotherus.net/family-data/images/" (name person-id) "/small.png")}] ))
 
 (defn format-life [{birth-time :time birth-city :city} {death-time :time death-city :city}]
   [:div
@@ -31,19 +37,18 @@
 (defn person-row [{person-name :name :keys [id images profession birth death]}]
   [:tr
    [:td id]
-   [:td (format-image (first images))]
-   [:td (format-name person-name) (if profession [:span ", " profession])]
-   [:td (format-life birth death)]])
-
+   [:td (format-image id (first images))]
+   [:td {:style { :width "9cm"}} (format-name person-name) (if profession [:span ", " profession])]
+   [:td {:style { :width "8cm"}} (format-life birth death)]])
 
 (defn person-list []
   (let [persons @(re-frame/subscribe [:filtered-persons])]
     [:table.even-odd
-     (into [:tbody] (->> persons (map person-row)))]))
+     (into [:tbody] (map person-row persons))]))
 
 (defn main-panel []
   [:div
-   [:h1 "List of Persons"]
+   [header]
    [:table
     [:tbody
      [:tr
