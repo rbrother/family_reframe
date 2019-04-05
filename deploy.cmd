4bde28a6b23474983d5203a5a22d92f656d44a82
Robert Brotherus <robert@iki.fi>
2019-04-05 22:54:26 +0300

    Remove "min" compilation option since produces errors (try to take back later)

--- deploy.cmd
+++ deploy.cmd
@@ -1,3 +1,5 @@
 CALL lein clean
-CALL lein cljsbuild once min
+REM the min-option seems to produce invalid call in (defn image...)
+REM CALL lein cljsbuild once min
+CALL lein cljsbuild once
 firebase deploy
